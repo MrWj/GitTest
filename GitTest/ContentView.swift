b05947366af8d14379d283fc2e04aee24aa773a5
万洁 <wanjieyululu@gmail.com>
2020-08-24 10:54:18 +0800

    提交文件

---
+++ GitTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GitTest
+//
+//  Created by 万洁 on 2020/8/24.
+//  Copyright © 2020 com.wjyll.www. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
