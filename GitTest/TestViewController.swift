afc371cdc0631c31dc7b67f0c6019ccd4a2ccbe5
万洁 <wanjieyululu@gmail.com>
2020-10-09 10:23:33 +0800

    dd

--- GitTest/TestViewController.swift
+++ GitTest/TestViewController.swift
@@ -14,6 +14,7 @@ class TestViewController: UIViewController {
         super.viewDidLoad()
         print("")
         // Do any additional setup after loading the view.
+        print("")
     }
     
 
