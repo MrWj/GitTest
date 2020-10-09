5e8e5eb29ec2b646b2c077b1f662a1a45be86e22
万洁 <wanjieyululu@gmail.com>
2020-10-09 10:21:23 +0800

    测试

--- GitTest/TestViewController.swift
+++ GitTest/TestViewController.swift
@@ -12,7 +12,7 @@ class TestViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print("")
         // Do any additional setup after loading the view.
     }
     
